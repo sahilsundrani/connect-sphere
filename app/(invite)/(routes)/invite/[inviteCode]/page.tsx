02e233a36b325a8c730c03ddc64c1e6e1cbc4609
Sahil Sundrani <sahilsundrani@Sahils-MacBook-Air.local>
2024-04-04 00:39:24 +0530

    implemented invite funtionality

---
+++ app/(invite)/(routes)/invite/[inviteCode]/page.tsx
@@ -0,0 +1,58 @@
+import { redirectToSignIn } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+import { db } from "@/lib/db";
+import { CurrentProfile } from "@/lib/current-profile";
+
+interface InviteCodePageProps {
+  params: {
+    inviteCode: string;
+  };
+};
+
+const InviteCodePage = async ({
+  params
+}: InviteCodePageProps) => {
+  const profile = await CurrentProfile();
+
+  if (!profile) {
+    return redirectToSignIn();
+  }
+
+  if (!params.inviteCode) {
+    return redirect("/");
+  }
+
+  //this can be optimized
+  const existingServer = await db.server.findFirst({
+    where: {
+      inviteCode: params.inviteCode,
+    }
+  });
+  let serverId = existingServer?.id;
+  const existingMember = await db.member.findFirst({
+    where: {
+        serverId,
+        profileId: profile.id,
+    }
+  });
+  if (existingMember && existingServer) {
+    return redirect(`/servers/${serverId}`);
+  }
+
+  const newMember = await db.member.create({
+    data: {
+        profileId: profile.id,
+        serverId,
+    }
+  });
+  // till here
+
+  if (newMember) {
+    return redirect(`/servers/${serverId}`);
+  }
+  
+  return null;
+}
+ 
+export default InviteCodePage;
