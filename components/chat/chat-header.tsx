9a1a53b7fe7e01b4d3600099587c3a9a681e9b97
Sahil Sundrani <sahilsundrani@Sahils-MacBook-Air.local>
2024-07-05 03:29:35 +0530

    added livekit for audio/video chat

--- components/chat/chat-header.tsx
+++ components/chat/chat-header.tsx
@@ -4,7 +4,7 @@ import { MobileToggle } from "@/components/mobile-toggle";
 import { UserAvatar } from "@/components/user-avatar";
 import { SocketIndicator } from "@/components/socket-indicator";
 
-// import { ChatVideoButton } from "./chat-video-button";
+import { ChatVideoButton } from "./chat-video-button";
 
 interface ChatHeaderProps {
   serverId: string;
@@ -19,13 +19,13 @@ export const ChatHeader = ({
   type,
   imageUrl
 }: ChatHeaderProps) => {
-  return (  
+  return (
     <div className="text-md font-semibold px-3 flex items-center h-12 border-neutral-200 dark:border-neutral-800 border-b-2">
       <MobileToggle serverId={serverId} />
       {type === "channel" && (
         <Hash className="w-5 h-5 text-zinc-500 dark:text-zinc-400 mr-2" />
-        )} 
-      {type === "conversation" && (  
+      )}
+      {type === "conversation" && (
         <UserAvatar 
           src={imageUrl}
           className="h-8 w-8 md:h-8 md:w-8 mr-2"
@@ -35,9 +35,9 @@ export const ChatHeader = ({
         {name}
       </p>
       <div className="ml-auto flex items-center">
-        {/* {type === "conversation" && (
+        {type === "conversation" && (
           <ChatVideoButton />
-        )} */}
+        )}
         <SocketIndicator />
       </div>
     </div>
